edd3fe63c1ddf44aee2335815b98e31d43a009cf
MendosDV <mendos.prod@gmail.com>
2025-09-20 00:00:51 -0400

    Complete Day 21

--- day_21_snake_game_part_2/main.py
+++ day_21_snake_game_part_2/main.py
@@ -27,13 +27,21 @@ while game_on:
     time.sleep(0.1)
     snake.move()
 
+    # If we touch the food
     if snake.head.distance(food) < 15:
+        snake.extend()
         score.update()
         food.refresh()
 
+    # If we touch the wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_on = False
         score.game_over()
 
+    # Did we touch tail
+    for segment in snake.segments[1:]:
+        if segment.head.distance(segment) < 10:
+            game_on = False
+            score.game_over()
 
 screen.exitonclick()
