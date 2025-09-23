cdaf6afbc8baf1678c0e84ed774926a6e763daee
MendosDV <mendos.prod@gmail.com>
2025-09-22 22:24:11 -0400

    Add saving in snake game

--- day_21_snake_game_part_2/main.py
+++ day_21_snake_game_part_2/main.py
@@ -29,19 +29,19 @@ while game_on:
 
     # If we touch the food
     if snake.head.distance(food) < 15:
-        snake.extend()
-        score.update()
         food.refresh()
+        snake.extend()
+        score.increase_score()
 
     # If we touch the wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     # Did we touch tail
     for segment in snake.segments[1:]:
-        if segment.head.distance(segment) < 10:
-            game_on = False
-            score.game_over()
+        if snake.head.distance(segment) < 10:
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
--- day_21_snake_game_part_2/scoreboard.py
+++ day_21_snake_game_part_2/scoreboard.py
@@ -10,16 +10,38 @@ class ScoreBoard(Turtle):
         self.penup()
         self.goto(0, 250)
         self.score = 0
+        self.high_score = 0
+        self.retrieve_save()
         self.show()
 
     def show(self):
         self.clear()
-        self.write(f"Score is {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score : {self.score} | High score : {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def update(self):
-        self.score += 1
         self.show()
 
     def game_over(self):
         self.goto(0,0)
         self.write("Game over", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.save_score()
+        self.score = 0
+        self.update()
+
+    def increase_score(self):
+        self.score += 1
+        self.update()
+
+    def save_score(self):
+        score = str(self.high_score)
+        with open("saving.txt", mode="w") as f:
+            f.write(score)
+
+    def retrieve_save(self):
+        with open("saving.txt", mode="r") as f:
+            score = f.read()
+            self.high_score = int(score)
