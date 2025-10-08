cb3fa7c2167afe3515d73de6ad9920b32127e343
MendosDV <mendos.prod@gmail.com>
2025-10-07 23:32:39 -0400

    Complete Day 32

--- day_32_birthday_wisher/main.py
+++ day_32_birthday_wisher/main.py
@@ -1,28 +1,42 @@
 import datetime as dt
-import smtplib
+import os
 import random
+import pandas as pd
+import smtplib
 
-MY_EMAIL = "XXX"
-MY_PASSWORD = "XXX"
+MY_EMAIL = 'mickaelriss6@gmail.com'
+MY_PASSWORD = "brgj bnnf lwps xkcc"
 
+# Get current day and month
 now = dt.datetime.now()
-weekday = now.weekday()
+today_month = now.month
+today_day = now.day
 
-if weekday == 1:
-    with open("quotes.txt", "r") as file:
-            quotes = file.readlines()
-            quote = random.choice(quotes)
+# Store birthdays in dict
+data = pd.read_csv("birthdays.csv")
+birthdays = {
+    (row["month"], row["day"]): row
+    for index, row in data.iterrows()
+}
 
-    print(quote)
+# If someone birthday create letter
+if (today_month, today_day) in birthdays:
+    letter = random.choice(os.listdir("./letter_templates"))
+    with open(f"./letter_templates/{letter}", "r") as file:
+        birthday_person = birthdays[(today_month, today_day)]
+        new_letter = file.read()
+        new_letter = new_letter.replace("[NAME]", birthday_person["name"])
 
-    try:
-        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.starttls()
-            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs="XXXX",
-                msg=f"Subject: Monday Motivation\n\n{quote}"
-            )
-    except Exception as e:
-        print(f"It's not working becauuse of {e}")
+# Send the letter generated
+# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+try:
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n{new_letter}"
+        )
+except Exception as e:
+    print(f"It's not working becauuse of {e}")
