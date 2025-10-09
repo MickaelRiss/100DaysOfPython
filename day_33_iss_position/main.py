50bd3c1067142c8ceab865ff8f4cf951c1d473a3
MendosDV <mendos.prod@gmail.com>
2025-10-08 22:51:30 -0400

    Complete Day 33

--- day_33_iss_position/main.py
+++ day_33_iss_position/main.py
@@ -1,26 +1,48 @@
-from tkinter import *
 import requests
+import datetime as dt
+import smtplib
+import time
 
-def get_quote():
-    response = requests.get("https://api.kanye.rest/")
+MY_EMAIL = "XXXXX@gmail.com"
+MY_PASSWORD = "XXXXXXXXXXX"
+MY_LAT = 45.501690
+MY_LNG = -73.567253
+
+def is_iss_close():
+    response = requests.get("http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
-    canvas.itemconfig(quote_text, text=data["quote"])
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+    iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LNG-5 <= iss_longitude <= MY_LNG+5:
+        return True
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
+def is_night():
+    params = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0
+    }
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=params)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = dt.datetime.now().hour
 
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
-
-window.mainloop()
+while True:
+    time.sleep(60)
+    if is_iss_close() and is_night():
+        with smtplib.SMTP("stmp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg="Subject:Look Up!\n\nThe ISS is right there"
+            )
