cbf13cf7cc09c888086c71fc220dbd1c62d0cefa
MendosDV <mendos.prod@gmail.com>
2025-10-16 22:24:43 -0400

    Start Day 37

---
+++ day_37_habbit_tracker/main.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+## CREATE USER
+# pixela_endpoint = "https://pixe.la/v1/users"
+# TOKEN = os.getenv("MY_TOKEN")
+#
+# user_params = {
+#     "token": TOKEN,
+#     "username": "Mickael",
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes"
+# }
+#
+# response = requests.post(url=pixela_endpoint, json=user_params)
