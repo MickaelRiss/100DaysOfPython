24951d9a51778297b9ccc20d67ddecd5e448bf5e
Mickael Riss <mendos.prod@gmail.com>
2025-10-29 23:22:14 -0400

    Start Day 39

---
+++ day_39_flight_deal_finder/flight_search.py
@@ -0,0 +1,11 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+class FlightSearch:
+    def __init__(self):
+        pass
+
+    def get_dest_code(self, city_name):
+        code = 'TESTING'
+        return code
---
+++ day_39_flight_deal_finder/main.py
@@ -0,0 +1,14 @@
+from data_manager import DataManager
+from flight_search import FlightSearch
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+
+if sheet_data[0]["iataCode"] == "":
+    flight_search = FlightSearch()
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_dest_code(row["city"])
+    print(sheet_data)
+
+    data_manager.destination_data = sheet_data
+    data_manager.update_deals()
