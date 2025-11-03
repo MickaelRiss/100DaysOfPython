3f67b44ffde8a6d6c59a667962594b35de0e94b0
Mickael Riss <mendos.prod@gmail.com>
2025-11-03 18:42:26 -0500

    Complete Day 45

--- day_45_beautiful_soup/main.py
+++ day_45_beautiful_soup/main.py
@@ -1,29 +1,14 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get("https://appbrewery.github.io/news.ycombinator.com/")
-web_page = response.text
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-soup = BeautifulSoup(markup=web_page, features="html.parser")
-# article_tag = soup.find(name="a", class_="storylink")
-# print(article_tag)
-# article_text = article_tag.get_text()
-# print(article_text)
-# article_link = article_tag.get("href")
-# print(article_link)
-# article_upvote = soup.find(name="span", class_="score").get_text()
-# print(article_upvote)
+response = requests.get(url=URL)
+website = response.text
+soup = BeautifulSoup(markup=website, features="html.parser")
+titles_tag = soup.find_all(name="h3", class_="title")
+titles = [title.get_text() for title in titles_tag]
 
-articles_tag = soup.find_all(name="a", class_="storylink")
-articles = [{"text": article.get_text(), "link": article.get("href")} for article in articles_tag]
-articles_upvotes = [int(vote.get_text().split()[0]) for vote in soup.find_all(name="span", class_="score")]
-
-highest_vote = max(articles_upvotes)
-highest_vote_index = articles_upvotes.index(highest_vote)
-
-for index, article in enumerate(articles):
-    if index == highest_vote_index:
-        article["vote"] = highest_vote
-        print(article)
-
-
+with open("movies.txt", "a") as file:
+    for title in reversed(titles):
+        file.write(f"{title}\n")
