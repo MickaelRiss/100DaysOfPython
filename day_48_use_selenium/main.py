daf7ad35695fd9af1ef7df8a7c0bfbcdcff7d099
Mickael Riss <mendos.prod@gmail.com>
2025-11-06 18:52:11 -0500

    Complete Day 48

--- day_48_use_selenium/main.py
+++ day_48_use_selenium/main.py
@@ -1,22 +1,44 @@
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from time import sleep, time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://secure-retreat-92358.herokuapp.com/")
+driver.get("https://ozh.github.io/cookieclicker/")
 
-first_name = driver.find_element(By.CLASS_NAME, value="top")
-last_name = driver.find_element(By.CLASS_NAME, value="middle")
-email = driver.find_element(By.CLASS_NAME, value="bottom")
+# Wait for page to load
+sleep(2)
 
-first_name.send_keys("Mickael")
-last_name.send_keys("Riss")
-email.send_keys("mickael.riss@yahoo.fr", Keys.ENTER)
+# By pass language
+try:
+    fr_language = driver.find_element(By.ID, value="langSelect-FR")
+    fr_language.click()
+except NoSuchElementException:
+    print("Language not find.")
 
-# search_input = driver.find_element(By.NAME, value="search")
-# search_input.send_keys("Python", Keys.ENTER)
+# Wait for page to load
+sleep(2)
+
+cookie = driver.find_element(By.ID, value="bigCookie")
+
+# Check purchase every 5sec
+timeout = time() + 5
+five_min = time() + 60 * 5
+click = True
+
+while click:
+    cookie.click()
+
+    if time() > timeout:
+        items = driver.find_elements(By.CSS_SELECTOR, value="#products .product")
+        for item in reversed(items):
+            if item.get_attribute("class") == "product unlocked enabled":
+                item.click()
+
+    if time() > five_min:
+        click = False
 
 driver.quit()
