89e7e88e9e644f1a5072f55e30f424ae97f081b6
Mickael Riss <mendos.prod@gmail.com>
2025-11-06 17:48:46 -0500

    First test with Selenium

--- day_48_use_selenium/main.py
+++ day_48_use_selenium/main.py
@@ -7,9 +7,21 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.python.org/")
-search_bar = driver.find_element(By.NAME, value="q")
-print(search_bar)
-button = driver.find_element(By.ID, value="submit")
-print(button)
+# search_bar = driver.find_element(By.NAME, value="q")
+# print(search_bar)
+# button = driver.find_element(By.ID, value="submit")
+# print(button)
+
+events_time = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+events_name = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+events = {}
+
+for n in range(len(events_time)):
+    events[n] = {
+        "time": events_time[n].text,
+        "name": events_name[n].text
+    }
+
+print(events)
 
 driver.quit()
